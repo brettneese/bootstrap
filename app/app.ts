c584ca246221d9d723e4205cde03d74bcceb48ef
Brett Neese <brett@neese.rocks>
2016-05-06 10:50:16 -0500

    first commit of bootstrap project

---
+++ app/app.ts
@@ -0,0 +1,37 @@
+/// <reference path='../typings/main.d.ts' />
+'use strict'
+
+/**
+ * Configure environmental variables
+ */
+require('dotenv').config({silent: true, path: '../.secrets'})
+
+/**
+ * Configure credentials for Dynasty
+ */
+var credentials = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+}
+
+/**
+ * Setting up Modules
+ */
+var _ = require('lodash'),
+    promise = require('bluebird'),
+    winston  = require('winston')
+
+    // dynasty = require('dynasty')(credentials),
+    // updates = dynasty.table('tracky-openshift'),
+    // request = promise.promisifyAll(require('request'))
+
+/**
+ * Configure logging
+ */
+require('winston-loggly')
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({ level: 'debug' }),
+    new (winston.transports.File)({ filename: '../.logs/app.log', level: 'info' })
+  ]
+})
